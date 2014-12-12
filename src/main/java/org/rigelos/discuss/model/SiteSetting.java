3b25d7a2e42ef9baf2ab2eda2c3abb89f7497e25
zhuruixian <zhuruixian@zhuruixian-PC>
2014-12-12 23:40:39 +0800

    add views

---
+++ src/main/java/org/rigelos/discuss/model/SiteSetting.java
@@ -0,0 +1,14 @@
+package org.rigelos.discuss.model;
+
+public class SiteSetting {
+
+	private String locale;
+
+	public String getLocale() {
+		return locale;
+	}
+
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
+}
