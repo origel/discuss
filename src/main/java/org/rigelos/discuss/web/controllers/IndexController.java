cf5deb48f0d54292eeda08356a8f6b23a0754ba3
zhuruixian <zhuruixian@zhuruixian-PC>
2014-12-21 12:10:40 +0800

    add index

--- src/main/java/org/rigelos/discuss/web/controllers/IndexController.java
+++ src/main/java/org/rigelos/discuss/web/controllers/IndexController.java
@@ -1,8 +1,14 @@
 package org.rigelos.discuss.web.controllers;
 
+import net.paoding.rose.web.Invocation;
 import net.paoding.rose.web.annotation.Path;
+import net.paoding.rose.web.annotation.rest.Get;
 
 @Path("/")
 public class IndexController extends AbstractCommonController {
 
+	@Get("")
+	public String index(Invocation inv){
+		return "index";
+	}
 }
