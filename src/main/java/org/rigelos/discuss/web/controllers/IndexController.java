eeff3e07bb2da792208a328472082f4d124f84ea
zhuruixian <zhuruixian@zhuruixian-PC>
2014-12-25 23:38:58 +0800

    topic

--- src/main/java/org/rigelos/discuss/web/controllers/IndexController.java
+++ src/main/java/org/rigelos/discuss/web/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package org.rigelos.discuss.web.controllers;
 
 import net.paoding.rose.web.Invocation;
+import net.paoding.rose.web.annotation.Param;
 import net.paoding.rose.web.annotation.Path;
 import net.paoding.rose.web.annotation.rest.Get;
 
@@ -10,5 +11,10 @@ public class IndexController extends AbstractCommonController {
 	@Get("")
 	public String index(Invocation inv){
 		return "index";
+	}
+	
+	@Get("topic-{topicId:[0-9]+}.html")
+	public String topic(@Param("topicId") int topicId, Invocation inv){
+		return "topic";
 	}
 }
