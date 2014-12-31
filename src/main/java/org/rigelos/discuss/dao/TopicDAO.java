e009bfab438abfe1145846e335e4d704385e436d
zhuruixian <zhuruixian@zhuruixian-PC>
2014-12-31 22:32:49 +0800

    add topic

--- src/main/java/org/rigelos/discuss/dao/TopicDAO.java
+++ src/main/java/org/rigelos/discuss/dao/TopicDAO.java
@@ -19,7 +19,7 @@ public interface TopicDAO {
     @SQL("update " + TABLE + " set status=0 where topicId=:topicId and status=1")
     public int remove(@SQLParam("topicId") long topicId);
 
-    @SQL("select " + FIELDS + " from " + TABLE + " where status=1 " +
+    @SQL("select " + FIELDS + " from " + TABLE + " where status=1 order by updateTime desc " +
          "#if(:size>0){ limit :offset,:size}")
     public List<Topic> select(@SQLParam("filter") TopicFilter filter, @SQLParam("offset") int offset, @SQLParam("size") int size);
 
@@ -27,7 +27,7 @@ public interface TopicDAO {
     public int getCount(@SQLParam("filter") TopicFilter filter);
 
     @ReturnGeneratedKeys
-    @SQL("insert into " + TABLE + FIELDS_REPLACE + " values(:topic.topicId,:topic.title,:topic.content,:topic.categoryId,:topic.userId,:topic.posts,:topic.views,:topic.likes,:topic.createTime,:topic.updateTime,:topic.status)")
+    @SQL("insert into " + TABLE + FIELDS_REPLACE + " values(:topic.topicId,:topic.title,:topic.content,:topic.categoryId,:topic.userId,:topic.posts,:topic.views,:topic.likes,now(),now(),:topic.status)")
     public int add(@SQLParam("topic") Topic topic);
 
     @SQL("update " + TABLE + " set title=:topic.title,content=:topic.content,categoryId=:topic.categoryId,userId=:topic.userId,posts=:topic.posts,views=:topic.views,likes=:topic.likes,createTime=:topic.createTime,updateTime=:topic.updateTime,status=:topic.status where topicId=:topic.topicId")
--- src/main/java/org/rigelos/discuss/web/controllers/IndexController.java
+++ src/main/java/org/rigelos/discuss/web/controllers/IndexController.java
@@ -1,7 +1,14 @@
 package org.rigelos.discuss.web.controllers;
 
+import java.util.List;
+
 import org.json.JSONObject;
 import org.rigelos.discuss.model.Topic;
+import org.rigelos.discuss.model.TopicFilter;
+import org.rigelos.discuss.util.JSONUtils;
+import org.rigelos.discuss.util.PageInfo;
+import org.rigelos.discuss.util.PageInfoBuilder;
+import org.springframework.web.bind.ServletRequestUtils;
 
 import net.paoding.rose.web.Invocation;
 import net.paoding.rose.web.annotation.Param;
@@ -11,19 +18,40 @@ import net.paoding.rose.web.annotation.rest.Get;
 @Path("/")
 public class IndexController extends AbstractCommonController {
 
+	private static int PAGE_SIZE = 20;
 	@Get("")
 	public String index(Invocation inv){
+		int page = ServletRequestUtils.getIntParameter(inv.getRequest(), "page", 1);
+		TopicFilter filter = new TopicFilter();
+		List<Topic> topicList = topicImpl.select(filter, 1, PAGE_SIZE);
+		inv.addModel("topicList", topicList);
+		
+		int total = topicImpl.getCount(filter);
+        PageInfo pageInfo = PageInfoBuilder.build("admin/product/list.html", total, page, PAGE_SIZE);
+        inv.addModel("pageInfo", pageInfo);
 		return "index";
 	}
 	
+	@Get("topics")
+	public JSONObject topics(TopicFilter filter, Invocation inv){
+		JSONObject result = new JSONObject();
+		List<Topic> topicList = topicImpl.select(filter, 1, PAGE_SIZE);
+		int total = topicImpl.getCount(filter);
+		JSONUtils.put(result, "total", total);
+		JSONUtils.put(result, "topicList", topicList);
+		return result;
+	}
+
 	@Get("topic-{topicId:[0-9]+}.html")
-	public String topic(@Param("topicId") int topicId, Invocation inv){
+	public String topic(@Param("topicId") long topicId, Invocation inv){
+		inv.addModel("topicId", topicId);
 		return "topic";
 	}
-	
+
 	public JSONObject addTopic(Topic topic, Invocation inv){
 		JSONObject result = new JSONObject();
 		
+		topicImpl.add(topic);
 		long ret = topicImpl.add(topic);
 		return result;
 	}
